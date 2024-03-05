353c2b8de68d9bb08122f1cd5e665624b40a1109
Dolly <abcddolly1234@gmail.com>
2024-03-05 22:38:58 +0530

    heroImage transition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,15 +18,20 @@ const Home = () => {
           <m.h2
             initial={{ x: "-150%" }}
             animate={{ x: "0%" }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            transition={{ duration: 0.5, delay: 0.75 }}
             className="text-4xl sm:text-7xl text-white font-bold"
           >
             I'm a Full Stack Web developer.
           </m.h2>
-          <p className="text-gray-500 py-4 max-w-md">
+          <m.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.75, ease: "easeIn" }}
+            className="text-gray-300 py-4 max-w-md"
+          >
             Hii, My name is Dolly Chauhan. I love to work on web applications
             using technologies like React, Tailwind, Express and Node.js.
-          </p>
+          </m.p>
 
           <div>
             <Link
@@ -41,13 +46,17 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
+        <m.div
+          initial={{ x: "200%" }}
+          animate={{ x: "0%" }}
+          transition={{ duration: 0.5, delay: 0.75 }}
+        >
           <img
             src={heroImage}
             alt="my profile"
             className=" rounded-xl opacity-75 mx-auto w-2/3 md:w-full transition duration-500 ease-in-out transform hover:opacity-100 hover:shadow-md hover:shadow-stone-200 hover:scale-110 "
           />
-        </div>
+        </m.div>
       </div>
     </m.main>
   );
